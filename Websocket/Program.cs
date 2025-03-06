a8937e4d4c89ca07f811b574bc15b374dfe6325b
killer_bigpoint <mineworkgaminglive@gmail.com>
2025-03-06 16:50:56 +0100

    Change websocket from windows form to console app

---
+++ Websocket/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DBD_Auto_Mapper_Websocket
+{
+    internal class Program
+    {
+        private static WebsocketListener server = null!;
+
+        static void Main(string[] args)
+        {
+            server = new WebsocketListener();
+            server.Initialize();
+        }
+    }
+}
---
+++ Websocket/WebsocketListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBD_Auto_Mapper_Websocket
+{
+    class WebsocketListener
+    {
+        const int port = 7788;
+
+        public void Initialize()
+        {
+            
+        }
+    }
+}
